67e67e78d976eae760788ec88ade2721f8c3be48
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-01-29 21:12:34 +0600

    Learning Javascript loops

---
+++ recap.js
@@ -0,0 +1,20 @@
+const amount = 200;
+// const numbers = [12, 32, 54, 65];
+// const numbers = [12, 98, 45, 63, 21, 72];
+
+// console.log(numbers);
+
+// for(const num of numbers){
+//     console.log(num);
+// }
+
+// const fruits = ["Orange", "Apple", "Banana", "Jackfruit", "Water"]
+// for(const fruit of fruits){
+//     console.log(fruit);
+//     console.log("I want to eat it.");
+// }
+
+const number = [7];
+if(number < 10){
+    console.log("I have got a small number")
+}
