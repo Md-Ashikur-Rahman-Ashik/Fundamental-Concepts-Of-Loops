9dd96040fa10a8bc18f51528626669bea555d68b
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-01-29 22:26:56 +0600

    Solved two tasks of while loop

--- recap.js
+++ recap.js
@@ -14,7 +14,48 @@ const amount = 200;
 //     console.log("I want to eat it.");
 // }
 
-const number = [7];
-if(number < 10){
-    console.log("I have got a small number")
+// const number = [7];
+// if(number < 10){
+//     console.log("I have got a small number")
+// }
+// console.log("Something new is cooking.");
+
+// let num = 0;
+// while(num < 5){
+//     console.log("Looping", num);
+//     // num = num + 1;
+//     // num += 1;
+//     num++;
+// }
+
+// let num = 1;
+// while(num <= 10){
+//     console.log(num);
+//     num++;
+// }
+
+// let num = 50;
+// while(num <= 100){
+//     console.log(num);
+//     num++;
+// }
+
+// let num = 1;
+// let sum = 0;
+// while(num <= 10){
+//     console.log(num);
+//     sum = sum + num;
+//     num++;
+//     console.log("The sum is:", sum);
+// }
+
+let num = 1;
+while(num <= 10){
+    console.log(num);
+    // Highlighting even numbers
+    if(num % 2 === 0){
+        console.log("Here, even number is:", num);
+    }
+    // Increasing the value of the number by adding one
+    num++;
 }
---
+++ tasks.js
@@ -0,0 +1,32 @@
+// First task
+// let num = 1;
+// while(num <= 60){
+//     console.log("I will invest at least 6 hrs every single day for next 60 days!");
+//     num++;
+// }
+
+// Second task
+
+// First subtask
+// num = 61;
+// while(num <= 100){
+//     console.log(num);
+//     // Highlighting odd numbers;
+//     if(num % 2 === 1){
+//         console.log("Here, odd number is:", num);
+//     }
+//     // Increasing the value of number by adding one every time
+//     num++;
+// }
+
+// Second subtask
+let num = 78;
+while(num <= 98){
+    console.log(num);
+    // Highlighting the even numbers
+    if(num % 2 === 0){
+        console.log("Here, even number is:", num);
+    }
+    // Increasing the number by adding one every single time
+    num++;
+}
