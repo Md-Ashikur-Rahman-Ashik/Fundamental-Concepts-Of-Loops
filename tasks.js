ac1ce7d6db75e15280ffbd70d44c86b5a3b57194
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-01-30 10:18:55 +0600

    All tasks of while loop done

--- tasks.js
+++ tasks.js
@@ -20,13 +20,62 @@
 // }
 
 // Second subtask
-let num = 78;
-while(num <= 98){
+// let num = 78;
+// while(num <= 98){
+//     console.log(num);
+//     // Highlighting the even numbers
+//     if(num % 2 === 0){
+//         console.log("Here, even number is:", num);
+//     }
+//     // Increasing the number by adding one every single time
+//     num++;
+// }
+
+// Third task
+
+// First subtask
+// let num = 81;
+// sum = 0;
+// while(num <= 131){
+//     console.log(num);
+//     // Highlighting the odd numbers
+//     if(num % 2 === 1){
+//         console.log("Here, the odd number is:", num);
+//         sum = sum + num;
+//         console.log("The sum is:", sum);
+//     }
+//     // Increasing the number by adding one each time
+//     num++;
+// }
+
+// Second subtask
+// let num = 206;
+// sum = 0;
+// while(num <= 311){
+//     console.log(num);
+//     // Highlighting the even numbers
+//     if(num % 2 === 0){
+//         console.log("Here, the even number is:", num);
+//         sum = sum + num;
+//         console.log("Here, the sum is:", sum);
+//     }
+//     // Incrementing the number by adding one each time
+//     num++;
+// }
+
+// Fourth task
+// let num = 1;
+// let start = 5;
+// let multiple;
+// while(num <= 10){
+//     multiple = (start * num);
+//     console.log(multiple);
+//     num++;
+// }
+
+// Fifth task
+let num = 21;
+while(num <= 21 && num >= 15){
     console.log(num);
-    // Highlighting the even numbers
-    if(num % 2 === 0){
-        console.log("Here, even number is:", num);
-    }
-    // Increasing the number by adding one every single time
-    num++;
+    num--;
 }
